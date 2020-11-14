ecbdcf856d50597dcce6e0c8835946bee60ca653
kohzandrew <kohzandrew@gmail.com>
2020-11-14 04:44:40 -0800

    The real coding began

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hey Anna</h1>
+    <p>Look how cool this website is.</p>
+    <p>Sup</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
